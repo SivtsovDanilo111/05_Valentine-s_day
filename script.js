701d093d293ffe025ca21c96550e18580bfa30f9
SivtsovDanilo111 <24a_sdp@liceum.ztu.edu.ua>
2025-02-18 12:14:43 +0200

    Остаточне виправлення помилки з кнопкою

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('btn_love_wishes').addEventListener('click', ()=> {
-document.getElementById('p-love-wishes').innerText = "Нехай кожен день приносить радість і тепло!"
+document.getElementById('love_wishes').innerText = "Нехай кожен день приносить радість і тепло!"
 })
